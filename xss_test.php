42b2d6cdef9ce967d8bffd26bc481daa82ca3b4e
LCatro <lcatro@sina.cn>
2016-09-10 18:54:29 +0800

    adject reflected xss analayis logic

--- xss_test.php
+++ xss_test.php
@@ -2,10 +2,7 @@
 <html>
 
     <script>
-        
-        REFLECTED_XSS_INJECT_DOM=1;
-        REFLECTED_XSS_INJECT_ELEMENT_OR_JAVASCRIPT=2;
-        
+        //  handler DOM 上变化来检测XSS
         
         function report(xss_detail_element) {
             console.log('WARNING! EVAL ELEMENT .. '+xss_detail_element.tagName+'  '+xss_detail_element.src);
@@ -97,15 +94,13 @@
             document.body.appendChild(script);
             */
         }
+    </script>
+    
+    <script>
+        //  反射XSS 检测部分
         
-        
-        /*
-        
-        
-            检测反射型XSS 逻辑..
-        
-        
-        */
+        REFLECTED_XSS_INJECT_DOM=1;
+        REFLECTED_XSS_INJECT_ELEMENT_OR_JAVASCRIPT=2;
         
         function split_url_parameter_value(parameter_key_value) {
             var offset_equal_flag=parameter_key_value.indexOf('=');
@@ -146,6 +141,21 @@
             return result;
         }
         
+        function split_string_space(input_string) {
+            var next_space_index=input_string.indexOf(' ');
+            var result=[];
+            
+            while (-1!=next_space_index) {
+                result.push(input_string.substr(0,next_space_index));
+                
+                input_string=input_string.substr(next_space_index+1);
+                next_space_index=input_string.indexOf(' ');
+            }
+            
+            result.push(input_string);
+            return result;
+        }
+        
         function analayis_reflected_parameter_xss(danger_parameter_list) {
             var result=[];
             
@@ -159,42 +169,16 @@
                     
                     if ('<'==eval_flag || '>'==eval_flag) {
                         analayis_index['reflected_type']=REFLECTED_XSS_INJECT_DOM;
+                        analayis_index['reflected_data']=danger_parameter_index;
                     } else if ('\''==eval_flag || '"'==eval_flag) {
                         analayis_index['reflected_type']=REFLECTED_XSS_INJECT_ELEMENT_OR_JAVASCRIPT;
+                        analayis_index['reflected_data']=split_string_space(danger_parameter_index);
                     }
-                    analayis_index['reflected_data']=danger_parameter_index;
                 }
                 result.push(analayis_index);
             }
             
             return result;
-        }
-         
-        function dom_child_element_recursion(element,parameter_analayis_information) {
-            //  遍历DOM 树会卡在页面渲染性能的瓶颈上..
-            /*
-            
-                TIPS -- XSS 过滤器会把注入到页面代码过滤掉,使得原来在URL 上的XSS 代码在DOM 上会改变
-                Example :
-                http://127.0.0.1/xss_test.php?xss_test_1=%3Cscript%3Ealert(%27xss%27);%3C/script%3E
-                                                    |
-                                                    v
-                                            <script> <//script>
-            
-            */
-            for (var parameter_index_ in parameter_analayis_information) {
-                var parameter_index=parameter_analayis_information[parameter_index_];
-                
-                if (REFLECTED_XSS_INJECT_DOM==parameter_index['reflected_type']) {
-                    var element_inner_html_code=element.innerHTML.toLowerCase();
-                    
-                    if (-1!=element_inner_html_code.indexOf(parameter_index['reflected_data']))
-                        return true;
-                } else if (REFLECTED_XSS_INJECT_ELEMENT_OR_JAVASCRIPT==parameter_index['reflected_type']) {
-                    
-                }
-            }
-            return false;
         }
         
         function find_first_eval_flag(parameter_value_string) {
@@ -219,6 +203,69 @@
         function check_insert_xss_code(parameter_value_string) {
             if (-1!=find_first_eval_flag(parameter_value_string))
                 return true;
+            return false;
+        }
+        
+        function resolve_element_tagname() {
+            
+        }
+        
+        function check_insert_xss_into_element(reflected_parameter) {
+            if (REFLECTED_XSS_INJECT_DOM==reflected_parameter['reflected_type']) {
+                var reg_express=new RegExp('<(\S*?) *>');
+                var element_list=reg_express.exec(reflected_parameter['reflected_data']);
+                console.log(reflected_parameter['reflected_data']);
+                console.log(element_list);
+                document.querySelector(reflected_parameter['reflected_data']);  //  TIPS : 解析出元素之后再去获取selector
+                
+            } else if (REFLECTED_XSS_INJECT_ELEMENT_OR_JAVASCRIPT==reflected_parameter['reflected_type']) {
+                var attribute_list=reflected_parameter['reflected_data'];
+                
+                if (attribute_list.length) {
+                    if (1==attribute_list.length) {  //  XSS 测试初期,使用" 或者' 号绕过元素的属性闭合
+                        var attribute_selector=document.querySelectorAll('[\\'+attribute_list[0]+']');
+                        /*
+                            选择含有' 和" 符号的HTML 元素,一般在XSS 初期测试的时候会通过注入' 和" 号看看能否开闭HTML 属性
+                            Example :
+                            
+                            <img src="%input%" />
+                                    |  
+                                    v
+                            正常情况下:<img src="123" />
+                            转码情况下:<img src="123&quet" />
+                            XSS 成功情况下:<img src="123" " />
+                        
+                        */
+                        
+                        if (attribute_selector.length) {
+                            console.log(attribute_selector);
+                        }
+                    } else {  //  URL 的参数中出现了多个HTML 元素属性..
+                        var attribute_query_selector_string='';
+                        
+                        for (var attribute_list_index in attribute_list) {
+                            var attribute_key_value=split_url_parameter_value(attribute_list[attribute_list_index]);
+                            
+                            if ('\''==attribute_key_value['key'] || 
+                                '"'==attribute_key_value['key'] ||
+                                undefined==attribute_key_value['key'])
+                                continue;
+                            
+                            attribute_query_selector_string+='[';
+                            attribute_query_selector_string+=attribute_key_value['key'];
+                            attribute_query_selector_string+=']';
+                        }
+                        
+                        //   WARNING ! 还没有好的办法找到event ..
+                        
+                        
+                        var attribute_selector=document.querySelectorAll(attribute_query_selector_string);
+                        console.log(attribute_query_selector_string);
+                        console.log(attribute_selector);
+                    }
+                }
+            }
+            
             return false;
         }
         
@@ -262,7 +309,7 @@
                                             v
                              arg=<script>alert('xss');<//script>
                 
-                最后利用DOM 树遍历找特征..
+                最后利用特征在document.querySelector 中发掘..
                 
             */
             
@@ -274,26 +321,46 @@
                     
                     decode_parameter_list.push(unescape(parameter_index['value']).toLowerCase().trim());
                 }
-                for (var index in decode_parameter_list)
-                    if (check_insert_xss_code(decode_parameter_list[index]))
-                        danger_parameter_value_list.push(decode_parameter_list[index]);
+                for (var decode_parameter_list_index in decode_parameter_list)
+                    if (check_insert_xss_code(decode_parameter_list[decode_parameter_list_index]))
+                        danger_parameter_value_list.push(decode_parameter_list[decode_parameter_list_index]);
                 
-                console.log(danger_parameter_value_list);
-                if (dom_child_element_recursion(document.body,analayis_reflected_parameter_xss(danger_parameter_value_list)))
-                    console.log('WARNING reflected XSS ');
+                if (danger_parameter_value_list.length) {
+                    var reflected_parameter_list=analayis_reflected_parameter_xss(danger_parameter_value_list);
+                    
+                    console.log(reflected_parameter_list);
+                    for (var reflected_parameter_list_index in reflected_parameter_list) {
+                        if (check_insert_xss_into_element(reflected_parameter_list[reflected_parameter_list_index])) {
+                            console.log('WARNING reflected XSS ');
+                        }
+                    }
+                }
             }
             return false;
         }
         
     </script>
+    
+    <script>
+                
+    </script>
    
     <body onload="check_reflected_xss()">
-       
+        
         <div id="xss_test_1"><!-- 反射XSS ,直接插入 <script> -->
             <?php
                 if (isset($_GET['xss_test_1']))
                     echo $_GET['xss_test_1'];
             ?>
+                            
+                            
+            <!-- test case -->
+                            
+            <img src="123" "/>
+            <img src="321" "/>
+            
+            <script>alert('xss');</script>
+                            
         </div>
         <div id="xss_test_2"><!-- 反射XSS ,直接插入 <img> -->
            
